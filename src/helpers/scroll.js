e74037621490bc7f32d86c55815a2a4513ae2173
Nastya Yermakova <ermkvnst@icloud.com>
2023-08-04 17:45:26 +0300

    refactored

--- src/helpers/scroll.js
+++ src/helpers/scroll.js
@@ -1,6 +1,25 @@
-export const scroll = () => {
-  window.scrollTo({
-    top: document.documentElement.scrollHeight,
-    behavior: 'smooth',
-  });
-};
+// import { gallery } from '../index';
+
+// const gallery = document.querySelector('#gallery');
+
+// export function scroll() {
+//   console.dir(gallery);
+//   if (!gallery.firstElementChild) {
+//     return;
+//   } else {
+//     const { height: cardHeight } =
+//       gallery.firstElementChild.getBoundingClientRect();
+
+//     window.scroll({
+//       top: cardHeight * 2,
+//       behavior: 'smooth',
+//     });
+//   }
+// }
+
+// export const scroll = () => {
+//   window.scrollTo({
+//     top: document.documentElement.scrollHeight,
+//     behavior: 'smooth',
+//   });
+// };
